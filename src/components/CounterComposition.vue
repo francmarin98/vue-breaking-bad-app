2c8ee3a465e4e202f57d132b9f55ba40f0c9e948
francmarin98 <francmarinc@gmail.com>
2023-03-03 22:55:06 -0500

    remove code and remember Composition and ScriptSetup

---
+++ src/components/CounterComposition.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'CounterComposition',
+  setup() {
+    const counter = ref(0);
+
+    const increment = () => {
+      counter.value += 1;
+    };
+
+    return {
+      counter,
+      increment,
+    };
+  },
+})
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1> Counter: {{counter}}</h1>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/CounterScriptSetup.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const counter = ref(0);
+
+const increment = () => {
+  counter.value += 1;
+};
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <h1> Counter: {{counter}}</h1>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
